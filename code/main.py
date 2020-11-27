9a04d3739bd2182adda6a6a00a4d379b0ff099d9
Arseniy Senokosov <senokosov.ao@phystech.edu>
2020-11-27 13:31:15 +0500

    minor fixes

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 # main module
 
+import time
 from random import choice
 
 import pygame as pg
@@ -14,18 +15,14 @@ def main():
     """
     Главная функция. Делает всё(почти).
     """
-    screen = pg.display.set_mode((window_width, window_height))
-    screen.fill(BLACK)
-    pg.display.update()
-
     clock = pg.time.Clock()
 
-    change_name_btn = ui.create_change_name_btn(screen)
+    change_name_btn = ui.create_change_name_btn()
 
-    players = player.read_players_from_file(screen)
+    players = player.read_players_from_file()
     current_player = player.define_current_player(players)
 
-    current_target = target.Target(hp=INITIAL_TARGET_HP + current_player.targets_killed * TARGET_HP_MULTIPLIER)
+    current_target = target.Target(hp=INITIAL_TARGET_HP + current_player.current_target_level * TARGET_HP_MULTIPLIER)
 
     back_pict = choice(back_pictures)  # выбирает начальный фон
 
@@ -33,22 +30,22 @@ def main():
 
     while not finished:
         clock.tick(FPS)
-        screen.fill(BLACK)
-        ui.draw_back_picture(back_pict, screen)
-        current_target.draw(screen)
+        ui.screen.fill(BLACK)
+        ui.draw_back_picture(back_pict, ui.screen)
+        current_target.draw(ui.screen)
         change_name_btn.draw()
-        current_player.draw_stats(screen)
+        current_player.draw_stats(ui.screen)
 
         for event in pg.event.get():
 
             change_name_btn.handle_event(event)
 
             if change_name_btn.clicked:
-                new_data, new_name = ui.change_player(screen)
+                new_data, new_name = ui.change_player()
                 change_name_btn.clicked = False
                 if new_data:
                     players, current_player = player.handle_new_data(new_name, players, current_player)
-                    current_target = target.Target(hp=target.calculate_hp(current_player.targets_killed))
+                    current_target = target.Target(hp=target.calculate_hp(current_player.current_target_level))
 
             if event.type == pg.QUIT:
                 finished = True
@@ -61,10 +58,11 @@ def main():
 
         if current_target.died:
             current_player.power_up()
-            current_target = target.Target(hp=target.calculate_hp(current_player.targets_killed))
+            current_target = target.Target(hp=target.calculate_hp(current_player.current_target_level))
         pg.display.update()
 
     current_player.last_player = True
+    current_player.last_login = time.time()
     player.write_players_to_file(players)
 
 
