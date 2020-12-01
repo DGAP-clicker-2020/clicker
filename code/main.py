4586c054c2740e75831259be008f040703a5085a
Arseniy Senokosov <senokosov.ao@phystech.edu>
2020-12-01 15:19:39 +0500

    fixes

--- code/main.py
+++ code/main.py
@@ -23,14 +23,12 @@ def main():
 
     current_target = target.Target(hp=target.calculate_hp(current_player.current_target))
 
-    back_picture = back_pictures[current_player.player_back_pict]  # выбирает задний фон
-
     finished = False
 
     while not finished:
         clock.tick(FPS)
         ui.screen.fill(BLACK)
-        ui.draw_back_picture(ui.screen)
+        ui.draw_back_picture(back_pictures[current_player.player_back_pict],ui.screen)
         current_target.draw(ui.screen)
         change_name_btn.draw()
         menu_open_btn.draw()
